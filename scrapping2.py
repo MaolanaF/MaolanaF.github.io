62b1c4d3dd6b4be10e6a732b1eac342ff029f756
MaolanaF <maolana.firmansyah.tif421@polban.ac.id>
2022-03-19 20:17:00 +0700

    Web Scrapping 2

---
+++ scrapping2.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from selenium import webdriver
+import urllib.request
+import json
+
+import requests
+
+PATH = "E:\Kuliah\SEMESTER 2\Proyek Pengembangan perangkat lunak dekstop\Pertemuan 6\Web scrapping 2\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://www.billboard.com/charts/billboard-korea-100/")
+
+x = datetime.now()
+
+billboard = []
+
+
+for song in driver.find_elements_by_class_name("o-chart-results-list-row"):
+    print(song.text)
+    for img in song.find_elements_by_class_name("c-lazy-image__img"):
+
+        billboard.append(
+            {"Ranking": song.text.split("\n")[0],
+             "Judul": song.text.split("\n")[1],
+             "Penyanyi": song.text.split("\n")[2],
+             "weeks_on_chart": song.text.split("\n")[5],
+             "waktu_scraping": x.strftime("%Y-%m-%d pukul %H:%M:%S"),
+             "Image": img.get_attribute("src")
+             }
+        )
+
+scrap = open("scrap.json", "w")
+json.dump(billboard, scrap, indent=6)
+scrap.close()
+driver.quit()
